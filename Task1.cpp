f4ff3d924b04a1c5c04a84ac1c591a0ea9315df7
SALMAN MUHAMMAD KHAN <k250017@nu.edu.pk>
2026-02-05 07:40:43 +0500

    Update Task1.cpp

--- Task1.cpp
+++ Task1.cpp
@@ -1,59 +1,62 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Book {
-    private : 
+private:
     string title;
     string author;
     double price;
 
-    public : 
+public:
     Book() {
-        title = " Untitled";
-        author = " anonymous";
+        title = "Untitled";
+        author = "Anonymous";
         price = 0.0;
-
     }
 
-    Book (string t, string a, double p) 
-    {
+    Book(string t, string a, double p) {
         title = t;
         author = a;
         price = p;
     }
 
-    Book (const &b) {
+    Book(const Book &b) {
         title = b.title;
         author = b.author;
         price = b.price;
     }
 
-    Book (string title) {
+    Book(string title) {
         this->title = title;
         author = "Unknown";
         price = 0.0;
     }
 
     void displayInfo() {
-        cout << "Book is" << title << endl;
-        cout << "Author is " << author << endl;
-        cout << "Price = " << price << endl;
+        cout << "Book title : " << title << endl;
+        cout << "Author     : " << author << endl;
+        cout << "Price      : " << price << endl;
+        cout << "------------------------" << endl;
     }
 };
 
-int main (){
+int main() {
     cout << "BOOK 1 info :-" << endl;
-    Book b1();
+    Book b1;
     b1.displayInfo();
-     cout << "BOOK 2 info :-" << endl;
-      Book b2("Harry Potter", "JK Rowling", 250.0);
-      b2.displayInfo();
-     cout << "BOOK 3 info :-" << endl;
-     Book b3(b2);
-     b3.displayInfo();
-     cout << "BOOK 4 info :-" << endl;
-     Book b4("Atomic Habits");
-     b4.displayInfo();
 
-     return 0;
+    cout << "BOOK 2 info :-" << endl;
+    Book b2("Harry Potter", "JK Rowling", 250.0);
+    b2.displayInfo();
+
+    cout << "BOOK 3 info :-" << endl;
+    Book b3(b2);
+    b3.displayInfo();
+
+    cout << "BOOK 4 info :-" << endl;
+    Book b4("Atomic Habits");
+    b4.displayInfo();
+
+    return 0;
 }
